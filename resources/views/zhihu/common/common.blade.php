d5440a702fa5b6ce14e89f179831a3200ceafb8f
亢超 <kangchaolove2009@163.com>
2018-04-02 15:59:50 +0800

    添加网站访问量统计代码

--- resources/views/zhihu/common/common.blade.php
+++ resources/views/zhihu/common/common.blade.php
@@ -1,1 +1,9 @@
-<p>我是common子视图include {{ $message }}<p>
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?471c47cdbba90d34c28ed180ba56ebff";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
